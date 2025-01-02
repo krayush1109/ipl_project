1741c453b4b8d0f5965eb6d0fd2e951e105d8244
username <wecp-dummy@gmail.com>
2025-01-02 17:13:32 +0000

    Day 2, 3 : Working Fine

--- backend/src/main/java/com/wecp/progressive/IplApplication.java
+++ backend/src/main/java/com/wecp/progressive/IplApplication.java
@@ -1,5 +1,6 @@
 package com.wecp.progressive;
 
+// hello world
 public class IplApplication {
     public static void main(String[] args) {
         System.out.println("Welcome to Ipl Progressive Project!");
