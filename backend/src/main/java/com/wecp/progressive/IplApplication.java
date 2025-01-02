4dc2edb0ae708651c1f1732e1bc60e6a58e37926
username <wecp-dummy@gmail.com>
2025-01-02 18:31:04 +0000

    Day 4 - Completed

--- backend/src/main/java/com/wecp/progressive/IplApplication.java
+++ backend/src/main/java/com/wecp/progressive/IplApplication.java
@@ -1,9 +1,13 @@
 package com.wecp.progressive;
 
-// hello world
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class IplApplication {
     public static void main(String[] args) {
         System.out.println("Welcome to Ipl Progressive Project!");
+        SpringApplication.run(IplApplication.class, args);
     }
 }
 
