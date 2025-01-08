70c87d814fadf2b78f82ad7012f1e4d2a73245c2
username <wecp-dummy@gmail.com>
2025-01-08 12:48:18 +0000

    Day 10: Completed

--- backend/src/main/java/com/wecp/progressive/repository/VoteRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/VoteRepository.java
@@ -1,5 +1,21 @@
 package com.wecp.progressive.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-public interface VoteRepository {
+import com.wecp.progressive.entity.Vote;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+    Long countByCategory(String category); 
+    
+    @Modifying 
+    @Query("DELETE FROM Vote v WHERE v.team.id = ?1") 
+    void deleteByTeamId(int teamId); 
+    
+    @Modifying 
+    @Query("DELETE FROM Vote v WHERE v.cricketer.id = ?1") 
+    void deleteByCricketerId(int cricketerId);
+
 }
--- backend/src/main/java/com/wecp/progressive/service/impl/VoteServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/VoteServiceImpl.java
@@ -1,5 +1,38 @@
 package com.wecp.progressive.service.impl;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.wecp.progressive.entity.Vote;
+import com.wecp.progressive.repository.VoteRepository;
+
+@Service
 public class VoteServiceImpl  {
 
+    @Autowired
+    private VoteRepository voteRepository;
+
+    
+    public ResponseEntity<List<Vote>> getAllVotes() { 
+        List<Vote> votes = voteRepository.findAll(); 
+        return ResponseEntity.ok(votes); 
+    } 
+    
+    public ResponseEntity<Integer> createVote(Vote vote) { 
+        Vote savedVote = voteRepository.save(vote); 
+        return ResponseEntity.status(201).body(savedVote.getVoteId()); 
+    } 
+    
+    public ResponseEntity<Map<String, Long>> getVotesCountOfAllCategories() { 
+        List<Vote> votes = voteRepository.findAll(); 
+        
+        Map<String, Long> countMap = votes.stream() 
+                        .collect(Collectors.groupingBy(Vote::getCategory, Collectors.counting())); 
+        return ResponseEntity.ok(countMap);
+    }
 }
