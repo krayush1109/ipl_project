ffc0a2029c51084378545b043d37289ae3adaf3b
username <wecp-dummy@gmail.com>
2025-01-10 05:35:39 +0000

    _Day 11: In Progres...

--- backend/src/main/java/com/wecp/progressive/controller/TicketBookingController.java
+++ backend/src/main/java/com/wecp/progressive/controller/TicketBookingController.java
@@ -1,25 +1,58 @@
 package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.TicketBooking;
+import com.wecp.progressive.service.TicketBookingService;
+import com.wecp.progressive.service.impl.TicketBookingServiceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/ticket")
 public class TicketBookingController {
 
-    public ResponseEntity<List<TicketBooking>> getAllBookings() {
-        return null;
-    }
+    @Autowired
+    private TicketBookingServiceImpl ticketBookingServiceImpl;
 
-    public ResponseEntity<Integer> createBooking(TicketBooking ticketBooking) {
-        return null;
+    @GetMapping
+    public ResponseEntity<List<TicketBooking>> getAllBookings() {        
+        List<TicketBooking> result = ticketBookingServiceImpl.getAllTicketBookings();
+        return new ResponseEntity<>(result, HttpStatus.OK) ;
+        // return null;
     }
-
-    public ResponseEntity<Void> cancelBooking(int bookingId) {
-        return null;
+    
+    @PostMapping
+    public ResponseEntity<Integer> createBooking(@RequestBody TicketBooking ticketBooking) {
+        
+        int result = ticketBookingServiceImpl.createBooking(ticketBooking);
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+        
+        // return null;
     }
-
-    public ResponseEntity<List<TicketBooking>> getBookingsByUserEmail(String email) {
-        return null;
+    
+    @DeleteMapping("/{bookingID}")
+    public ResponseEntity<Void> cancelBooking(@PathVariable int bookingId) {
+        ticketBookingServiceImpl.cancelBooking(bookingId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        // return null;
+    }
+    
+    @GetMapping("/user/{email}")
+    public ResponseEntity<List<TicketBooking>> getBookingsByUserEmail(@PathVariable String email) {
+        
+        List<TicketBooking> result = ticketBookingServiceImpl.getBookingsByUserEmail(email);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+        
+        // return null;
     }
 }
--- backend/src/main/java/com/wecp/progressive/repository/TicketBookingRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/TicketBookingRepository.java
@@ -1,5 +1,40 @@
 package com.wecp.progressive.repository;
 
+import java.util.List;
 
-public interface TicketBookingRepository {
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.wecp.progressive.entity.TicketBooking;
+
+@Repository
+public interface TicketBookingRepository extends JpaRepository<TicketBooking, Integer> {
+
+    // List<TicketBooking> findByEmail(String email);
+
+    // @Modifying 
+    // @Query("DELETE FROM TicketBooking tb WHERE tb.match.id = :matchId") 
+    // void deleteByMatchId(@Param("matchId") int matchId); 
+    
+    // @Modifying 
+    // @Query("DELETE FROM TicketBooking tb WHERE tb.match.team1.id = :teamId OR tb.match.team2.id = :teamId") 
+    // void deleteByTeamId(@Param("teamId") int teamId);
+
+    List<TicketBooking> findByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM TicketBooking t WHERE t.match.matchId IN (SELECT m.matchId FROM matches m WHERE m.firstTeam.teamId = :teamId OR m.secondTeam.teamId = :teamId)")
+    void deleteByTeamId(@Param("teamId") int teamId);
+    
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM TicketBooking t WHERE t.match.matchId IN :matchId")
+    void deleteByMatchId(@Param("matchId") int matchId);
+
 }
